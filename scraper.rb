103f1e920f433c74414918849962e4d684fc3a36
Tony Bowden <tony@tmtm.com>
2015-05-14 22:14:03 +0100

    parse dates

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,7 @@ require 'scraperwiki'
 require 'nokogiri'
 require 'date'
 require 'open-uri'
+require 'date'
 
 require 'colorize'
 require 'pry'
@@ -16,6 +17,10 @@ def noko(url)
   Nokogiri::HTML(open(url).read) 
 end
 
+def datefrom(date)
+  Date.parse(date)
+end
+
 @BASE = 'http://www.consellgeneral.ad/ca/composicio-actual/consellers-generals'
 url = @BASE
 
@@ -24,22 +29,20 @@ added = 0
 
 page.css('div#content-core div.tileItem').drop(1).each do |p|
   p_url = p.at_css('h3 a/@href').text
-  # binding.pry
   dep = noko(p_url)
-  # partido = block.xpath('.//li/strong[contains(.,"Partido")]/../text()').text.strip
-  # binding.pry
   data = { 
     id: p_url.split('/').last,
     name: dep.at_css('h1#parent-fieldname-title').text.strip,
     area: dep.at_css('div.electe span').text.strip,
     party: dep.at_css('div.grup span').text.strip,
     email: dep.at_css('div.email span').text.strip,
-    birth_date: dep.at_css('div.datanaixement span').text.strip,
+    birth_date: datefrom(dep.at_css('div.datanaixement span').text.strip).to_s,
     image: dep.at_css('div.foto img/@src').text,
-    start_date: dep.at_css('div.dataeleccio span').text.strip,
+    start_date: datefrom(dep.at_css('div.dataeleccio span').text.strip).to_s,
     term: 2015,
     source: p_url,
   }
+  puts data
   added += 1
   ScraperWiki.save_sqlite([:name, :term], data)
 end
