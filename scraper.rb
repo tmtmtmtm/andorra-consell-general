6efa5328e8d2a57e646a32e4fdc2f52d6863f665
Tony Bowden <tony@tmtm.com>
2016-11-29 10:10:19 +0000

    Extract scrape_member method

--- scraper.rb
+++ scraper.rb
@@ -24,24 +24,28 @@ def scrape_list(url)
 
   page.css('div#content-core div.tileItem').drop(1).each do |p|
     p_url = p.at_css('h3 a/@href').text
-    dep = noko(p_url)
-    data = {
-      id: p_url.split('/').last,
-      name: dep.at_css('h1#parent-fieldname-title').text.strip,
-      area: dep.at_css('div.electe span').text.strip,
-      party: dep.at_css('div.grup span').text.strip,
-      email: dep.at_css('div.email span').text.strip,
-      birth_date: datefrom(dep.at_css('div.datanaixement span').text.strip).to_s,
-      image: dep.at_css('div.foto img/@src').text,
-      election_date: datefrom(dep.at_css('div.dataeleccio span').text.strip).to_s,
-      term: 2015,
-      source: p_url,
-    }
-    puts data
+    scrape_member(p_url)
     added += 1
-    ScraperWiki.save_sqlite([:id, :term], data)
   end
   puts "  Added #{added} members"
+end
+
+def scrape_member(url)
+  dep = noko(url)
+  data = {
+    id: url.split('/').last,
+    name: dep.at_css('h1#parent-fieldname-title').text.strip,
+    area: dep.at_css('div.electe span').text.strip,
+    party: dep.at_css('div.grup span').text.strip,
+    email: dep.at_css('div.email span').text.strip,
+    birth_date: datefrom(dep.at_css('div.datanaixement span').text.strip).to_s,
+    image: dep.at_css('div.foto img/@src').text,
+    election_date: datefrom(dep.at_css('div.dataeleccio span').text.strip).to_s,
+    term: 2015,
+    source: url,
+  }
+  puts data
+  ScraperWiki.save_sqlite([:id, :term], data)
 end
 
 scrape_list 'http://www.consellgeneral.ad/ca/composicio-actual/consellers-generals'
